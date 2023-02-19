3f422b8ed5832199ed12dbd3c47a2fe00a056504
Vladislav Cherepanov <V.V.Cherepanov@bk.ru>
2023-02-19 20:59:07 +0500

    a little experiment

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -116,7 +116,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static_backend')
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.permissions.AllowAny',
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 6,
